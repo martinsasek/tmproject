5b63f0418388b58bb27eda378cb51995e19e3638
Martin Sasek <martin.sasek@email.cz>
2015-06-23 07:54:55 +0200

    CHANGE: @Get service in MessageRestService now returns given user in JSON format .

--- src/main/java/cz/mojespolecnost/tmproject/rest/MessageRestService.java
+++ src/main/java/cz/mojespolecnost/tmproject/rest/MessageRestService.java
@@ -5,6 +5,7 @@ import cz.mojespolecnost.tmproject.persistence.UserDO;
 import java.io.IOException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -48,13 +49,11 @@ public class MessageRestService {
 //	}
         
 	@GET
-	@Path("/firstUser")
+	@Path("/user/{id}")
         @Produces(MediaType.APPLICATION_JSON)
-	public UserDO printMessage() {        
-                
-                
+	public UserDO printMessage(@PathParam("id") int id) {        
 		
-		return conn.getUserByID(1);
+		return conn.getUserByID(id);
 
 	}      
         
